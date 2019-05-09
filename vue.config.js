605e45e9221cad1aa37836ff6ef00ec8d4ce3828
xxred <xxred0324@qq.com>
2019-05-09 18:36:19 +0800

    vue.config.js配置文件publicPath根据运行环境设置

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,10 @@ const path = require('path')
 function resolve(dir) {
   return path.join(__dirname, '.', dir)
 }
+
+// development、production。生产模式使用dist路径
+const publicPath = process.env.NODE_ENV === 'production' ? '/dist' : '/'
+
 module.exports = {
   chainWebpack: config => {
     config.module.rules.delete('svg') // 重点:删除默认配置中处理svg
@@ -22,5 +26,8 @@ module.exports = {
   devServer: {
     proxy: 'https://localhost:44336/',
     https: false
-  }
+  },
+  publicPath: publicPath,
+  // 导入vue包含编译器 https://cli.vuejs.org/zh/config/#runtimecompiler
+  runtimeCompiler: true
 }
