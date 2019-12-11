2389ec39787927f79073e0ebe143e355f63043a1
xxred <xxred0324@qq.com>
2019-12-11 15:12:07 +0800

    添加请求拦截，错误信息友好提示

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,16 +1,14 @@
 import axios from 'axios'
-import {
-  Message
-} from 'element-ui'
+import { Message, MessageBox } from 'element-ui'
 import store from '../store'
-import {
-  getToken
-} from '../utils/auth'
 
 // create an axios instance
 const service = axios.create({
   timeout: 50000 // request timeout
 })
+
+// 是否正在弹窗
+let isLoginTimeout = false
 
 // request interceptor
 service.interceptors.request.use(
@@ -31,51 +29,74 @@ service.interceptors.request.use(
   },
   error => {
     // Do something with request error
-    console.log(error) // for debug
+    // console.log(error) // for debug
+    Message({
+      message: error,
+      type: 'error',
+      duration: 5 * 1000
+    })
     Promise.reject(error)
   }
 )
 
 // response interceptor
 service.interceptors.response.use(
-  response => response,
+  // response => response,
   /**
    * 下面的注释为通过在response里，自定义code来标示请求状态
    * 当code返回如下情况则说明权限有问题，登出并返回到登录页
    * 如想通过 xmlhttprequest 来状态码标识 逻辑可写在下面error中
    * 以下代码均为样例，请结合自生需求加以修改，若不需要，则可删除
    */
-  // response => {
-  //   const res = response.data
-  //   if (res.code !== 20000) {
-  //     Message({
-  //       message: res.message,
-  //       type: 'error',
-  //       duration: 5 * 1000
-  //     })
-  //     // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
-  //     if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-  //       // 请自行在引入 MessageBox
-  //       // import { Message, MessageBox } from 'element-ui'
-  //       MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
-  //         confirmButtonText: '重新登录',
-  //         cancelButtonText: '取消',
-  //         type: 'warning'
-  //       }).then(() => {
-  //         store.dispatch('FedLogOut').then(() => {
-  //           location.reload() // 为了重新实例化vue-router对象 避免bug
-  //         })
-  //       })
-  //     }
-  //     return Promise.reject('error')
-  //   } else {
-  //     return response.data
-  //   }
-  // },
+  response => {
+    const res = response.data
+    if (res.Status && res.Status !== 0) {
+      if (res.Status >= 500) {
+        Message({
+          message: '后端服务错误',
+          type: 'error',
+          duration: 5 * 1000
+        })
+        return Promise.reject('后端服务错误')
+      } else if (res.Status === 203) {
+        // 请自行在引入 MessageBox
+        // import { Message, MessageBox } from 'element-ui'
+        // 如果已弹窗，不重复弹窗
+        if (!isLoginTimeout) {
+          isLoginTimeout = true
+          MessageBox.confirm(
+            '你已被登出或者登陆失效，可以取消继续留在该页面，或者重新登录',
+            '确定登出',
+            {
+              confirmButtonText: '重新登录',
+              cancelButtonText: '取消',
+              type: 'warning'
+            }
+          ).then(() => {
+            isLoginTimeout = false
+            store.dispatch('FedLogOut').then(() => {
+              location.reload() // 为了重新实例化vue-router对象 避免bug
+            })
+          })
+        } else {
+          return response
+        }
+      } else {
+        Message({
+          message: res.Msg,
+          type: 'error',
+          duration: 5 * 1000
+        })
+        return Promise.reject(res.Msg)
+      }
+    } else {
+      return response
+    }
+  },
   error => {
-    console.log('err' + error) // for debug
+    // console.log('err' + error) // for debug
     Message({
-      message: error.message,
+      message: '请求错误',
       type: 'error',
       duration: 5 * 1000
     })
