9e9d8f6512a0c785ea8e3cd9e439b28f9667d777
笑笑 <xxred0324@qq.com>
2020-11-04 19:05:35 +0800

    node-sass改为dart-sass

--- src/apiSettings.js
+++ src/apiSettings.js
@@ -11,9 +11,8 @@ const route = {
 export default settings => {
   settings.baseUrl =
   // ''
-  // 'http://localhost:44336'
+  'http://localhost:32768/'
   // 'http://localhost:7877'
-  'http://ids4.winoble.cn'
   settings.login = login
   settings.route = route
   return settings
